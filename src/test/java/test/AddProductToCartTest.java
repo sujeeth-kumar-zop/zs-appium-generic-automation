6bd47f875b04af1511b2d3d4be4cc55a95f4117f
AnushkaRawatZ <anushkarawat@zopsmart.com>
2024-11-22 17:44:04 +0530

    feature/add-to-cart-test|| Anushka Rawat|| Removed unused package imports

--- src/test/java/test/AddProductToCartTest.java
+++ src/test/java/test/AddProductToCartTest.java
@@ -1,20 +1,13 @@
 package test;
 
-import com.zs.constants.Constants;
 import com.zs.pages.common.Flows;
-import com.zs.pages.common.ProductsPage;
-import com.zs.pages.tamimi.SelectStorePage;
-import com.zs.pages.vijetha.LoginPageVijetha;
 import com.zs.utils.CommonUtils;
 import base.BaseTest;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import static org.testng.Assert.assertTrue;
 
