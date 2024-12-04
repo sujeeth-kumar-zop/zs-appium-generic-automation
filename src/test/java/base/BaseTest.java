642b14fb5033e4cf000499e795327a1e041a5033
AnushkaRawatZ <anushkarawat@zopsmart.com>
2024-12-04 18:11:26 +0530

    feature/searchTestOtherApps || Anushka || added timeout capability as 90000

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -72,6 +72,7 @@ public class BaseTest {
         cap = new DesiredCapabilities();
         cap.setCapability("platformName", "Android");
         cap.setCapability("appium:automationName", "uiautomator2");
+        cap.setCapability("uiautomator2ServerInstallTimeout", 90000);
 
         //fetch the app path from the configuration file
         String appPathKey = appName + ".apk.path";
