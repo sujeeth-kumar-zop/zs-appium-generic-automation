0affcc872f3640d0a9ab8658b9d652397bbdddea
AnushkaRawatZ <anushkarawat@zopsmart.com>
2024-12-19 17:32:54 +0530

    feature/editProfile|| Anushka Rawat|| Added logs

--- src/test/java/test/EditProfileTest.java
+++ src/test/java/test/EditProfileTest.java
@@ -6,6 +6,7 @@ import com.zs.pages.common.LoginPage;
 import com.zs.pages.common.ProfilePage;
 import com.zs.pages.tamimi.ProfilePageTamimi;
 import com.zs.utils.CommonUtils;
+import com.zs.utils.LoggerUtil;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -31,12 +32,19 @@ public class EditProfileTest extends BaseTest {
         ProfilePageTamimi profilePageTamimi = new ProfilePageTamimi(driver,wait);
 
         commonUtils.navigateToHome(appName);
+        LoggerUtil.logInfo("Navigating to home");
         loginPage.clickOnProfileIcon(appName);
+        LoggerUtil.logInfo("Clicked on the profile icon");
         profilePage.clickEditBtn(appName);
+        LoggerUtil.logInfo("Clicked on the Edit Button");
         profilePageTamimi.selectSalutation(Constants.TAMIMI_SALUTATION);
+        LoggerUtil.logInfo("Selected the salutation");
         profilePageTamimi.enterDetails(Constants.TAMIMI_FNAME, Constants.TAMIMI_LNAME, Constants.TAMIMI_EMAIL);
+        LoggerUtil.logInfo("Entered details and clicked on submit");
         WebElement successLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(CommonUtils.getProfileLocator(appName, "editProfileSuccessfulLabel")));
         assertEquals("Profile details updated successfully.", successLabel.getText());
+        LoggerUtil.logPass("Edit Profile Test Passed");
         driver.findElement(CommonUtils.getProfileLocator(appName, "okBtn")).click();
+        LoggerUtil.logInfo("Clicked on Ok");
     }
 }
