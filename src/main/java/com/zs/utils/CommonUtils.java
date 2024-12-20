f2c83962d07455cd4120ae492bb27ccbf9abc6a0
AnushkaRawatZ <anushkarawat@zopsmart.com>
2024-12-20 15:07:33 +0530

    feature/logout|| Anushka Rawat|| Increased the explicit wait time to check for homepage

--- src/main/java/com/zs/utils/CommonUtils.java
+++ src/main/java/com/zs/utils/CommonUtils.java
@@ -189,7 +189,7 @@ public class CommonUtils{
 
         int maxRetries = 10;
         int attempts = 0;
-        WebDriverWait shortWait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        WebDriverWait shortWait = new WebDriverWait(driver, Duration.ofSeconds(3));
         while (attempts < maxRetries) {
             try {
 
