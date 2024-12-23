6aa48c0feeaaa56455598cf2e9b4e504ccd26292
AnushkaRawatZ <anushkarawat@zopsmart.com>
2024-12-23 15:53:45 +0530

    feature/validateMaxProductQuantity|| Anushka Rawat|| Added productCount variable

--- src/main/java/com/zs/pages/common/HomePage.java
+++ src/main/java/com/zs/pages/common/HomePage.java
@@ -64,7 +64,7 @@ public class HomePage {
     public String fetchRandomProduct(String appName){
         LoggerUtil.logInfo("Fetching a random product to assert search.");
         List<WebElement> productsList = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(CommonUtils.getHomePageLocator(appName,"productsInSearch")));
-
+        int productCount = productsList.size();
         int randomNumber= ThreadLocalRandom.current().nextInt(0, productCount);
         LoggerUtil.logInfo("The random product's index is "+randomNumber);
         if(Constants.BRIMBARY.equals(appName)){
