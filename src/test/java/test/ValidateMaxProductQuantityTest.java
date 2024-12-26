bf45151a1b9804f4cde6db23f0a94840eb8bc06f
AnushkaRawatZ <anushkarawat@zopsmart.com>
2024-12-26 16:13:30 +0530

    feature/validateMaxProductQuantity|| Anushka Rawat|| Refactored code to increase readability

--- src/test/java/test/ValidateMaxProductQuantityTest.java
+++ src/test/java/test/ValidateMaxProductQuantityTest.java
@@ -26,9 +26,9 @@ public class ValidateMaxProductQuantityTest extends BaseTest {
 
         // Set the ExtentTest object in LoggerUtil to log to both ExtentReport and Log4j
         LoggerUtil.setExtentTest(ExtentReport.getTest());
-
         AndroidDriver driver = BaseTest.getDriver();
         WebDriverWait wait = BaseTest.getWait();
+
         CommonUtils commonUtils=new CommonUtils(driver,wait);
         HomePage homePage=new HomePage(driver,wait);
         ProductsPage productsPage=new ProductsPage(driver,wait);
@@ -40,12 +40,11 @@ public class ValidateMaxProductQuantityTest extends BaseTest {
         }
         commonUtils.navigateToHome(appName);
         homePage.enterTextInSearchBar(appName, Constants.TAMIMI_WATER);
-        wait.until(ExpectedConditions.elementToBeClickable(CommonUtils.getHomePageLocator(appName, "searchBar"))).sendKeys(Keys.RETURN);
-        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"Bottled Drinking Water-40×330ML\").instance(0))")).click();
-
+        commonUtils.enterVal(CommonUtils.getHomePageLocator(appName, "searchBar"), Keys.RETURN);
+        commonUtils.scrollAndClick(Constants.TAMIMI_WATER_PRODUCT);
         for(int i=0;i<5;i++){
-            wait.until(ExpectedConditions.elementToBeClickable(CommonUtils.getProductPageLocator(appName,"addToCartBtn"))).click();
+            commonUtils.click(CommonUtils.getProductPageLocator(appName,"addToCartBtn"));
         }
-        assertTrue("Warning was not displayed",wait.until(ExpectedConditions.visibilityOfElementLocated(CommonUtils.generateLocator(Constants.TAMIMI_PRODUCT_LIMIT_WARNING))).isDisplayed());
+        assertTrue("Warning was not displayed",commonUtils.isElementVisible(CommonUtils.generateLocator(Constants.TAMIMI_PRODUCT_LIMIT_WARNING)));
     }
 }
