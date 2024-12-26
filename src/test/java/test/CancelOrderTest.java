a4eb5af6aa659f5409724d8fc46b54122bf56191
AnushkaRawatZ <anushkarawat@zopsmart.com>
2024-12-26 15:29:04 +0530

    feature/cancelOrderTest|| Anushka Rawat|| Refactored test class to reduce code and make it readable

--- src/test/java/test/CancelOrderTest.java
+++ src/test/java/test/CancelOrderTest.java
@@ -2,6 +2,7 @@ package test;
 
 import base.BaseTest;
 import com.zs.constants.Constants;
+import com.zs.pages.common.Flows;
 import com.zs.utils.CommonUtils;
 import com.zs.utils.LoggerUtil;
 import com.zs.utils.OrderManager;
@@ -22,7 +23,7 @@ import static org.testng.AssertJUnit.assertTrue;
 
 public class CancelOrderTest extends BaseTest {
 
-    @Test(groups = {"regression"}, dependsOnGroups = {"checkout"})
+    @Test(groups = {"regression"}, dependsOnGroups = {"checkout", "login"})
     @Parameters("appName")
     public void cancelOrderTest(@Optional String appName) throws InterruptedException {
 
@@ -31,50 +32,22 @@ public class CancelOrderTest extends BaseTest {
         WebDriverWait wait= BaseTest.getWait();
 
         CommonUtils commonUtils=new CommonUtils(driver,wait);
+        Flows flows = new Flows(driver, wait);
 
         commonUtils.navigateToHome(appName);
-        LoggerUtil.logInfo("Navigating to home page");
-        wait.until(ExpectedConditions.elementToBeClickable(CommonUtils.getLoginLocator(appName, "profileIcon"))).click();
+
+        commonUtils.click(CommonUtils.getLoginLocator(appName, "profileIcon"));
         LoggerUtil.logInfo("Clicked on the Profile Icon");
-        wait.until(ExpectedConditions.elementToBeClickable(CommonUtils.generateLocator(Constants.ORDERS))).click();
+
+        commonUtils.click(CommonUtils.generateLocator(Constants.ORDERS));
         LoggerUtil.logInfo("Clicked on My Orders");
 
         List<String> orderNums = OrderManager.getOrderIds();
 
-        String order1Loc = CommonUtils.generateLocator("#"+orderNums.get(0)).toString();
-        String order1XpathValue = order1Loc.substring(order1Loc.indexOf(": ") + 2);
-        LoggerUtil.logInfo("Found the order number of order 1");
+        List<String> orderStatus = flows.cancelOrders(orderNums);
 
-        String order2Loc = CommonUtils.generateLocator("#"+orderNums.get(1)).toString();
-        String order2XpathValue = order2Loc.substring(order2Loc.indexOf(": ") + 2);
-        LoggerUtil.logInfo("Found the order number of order 2");
-
-        WebElement order1Parent = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(order1XpathValue+Constants.PARENT_LOC)));
-        LoggerUtil.logInfo("Found the parent element of order1");
-        WebElement order2Parent = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(order2XpathValue+Constants.PARENT_LOC)));
-        LoggerUtil.logInfo("Found the parent element of order2");
-
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(order1XpathValue+Constants.PARENT_LOC+Constants.FOURTH_CHILD)))).click();
-        LoggerUtil.logInfo("Clicked on the 4th child of parent of order 1: Kebab Menu");
-
-        wait.until(ExpectedConditions.elementToBeClickable(CommonUtils.generateLocator(Constants.CANCEL))).click();
-        LoggerUtil.logInfo("Clicked on Cancel for Order 1");
-
-        System.out.println(order2XpathValue+Constants.PARENT_LOC+Constants.FOURTH_CHILD);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(order2XpathValue + Constants.PARENT_LOC + Constants.FOURTH_CHILD)));
-
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(order2XpathValue+Constants.PARENT_LOC+Constants.FOURTH_CHILD)))).click();
-        LoggerUtil.logInfo("Clicked on the 4th child of parent of order 2: Kebab Menu");
-
-        wait.until(ExpectedConditions.elementToBeClickable(CommonUtils.generateLocator(Constants.CANCEL))).click();
-        LoggerUtil.logInfo("Clicked on Cancel for Order 2");
-
-        WebElement cancelLabel1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(order1XpathValue+Constants.PARENT_LOC+Constants.EIGHTH_CHILD)));
-        WebElement cancelLabel2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(order2XpathValue+Constants.PARENT_LOC+Constants.EIGHTH_CHILD)));
-
-        assertTrue("Order 1 did not get cancelled",cancelLabel1.getText().equalsIgnoreCase("Cancelled"));
-        assertTrue("Order 2 did not get cancelled",cancelLabel2.getText().equalsIgnoreCase("Cancelled"));
-
+        assertTrue("Order 1 did not get cancelled",orderStatus.get(0).equalsIgnoreCase("Cancelled"));
+        assertTrue("Order 2 did not get cancelled",orderStatus.get(1).equalsIgnoreCase("Cancelled"));
     }
 }
 
