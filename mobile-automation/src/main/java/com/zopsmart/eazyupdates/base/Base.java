24882b4b1a6eb770c3118bd5a577d8874b576c47
pritamH <priam.horo@zopsmart.com>
2025-08-21 17:13:45 +0530

    Refactored base class for  pipeline exhecution

--- mobile-automation/src/main/java/com/zopsmart/eazyupdates/base/Base.java
+++ mobile-automation/src/main/java/com/zopsmart/eazyupdates/base/Base.java
@@ -10,6 +10,7 @@ import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.options.XCUITestOptions;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.testng.annotations.*;
 
 import java.io.FileInputStream;
@@ -49,8 +50,9 @@ public class Base {
         appiumServiceBuilder = new AppiumServiceBuilder()
                 .withIPAddress("127.0.0.1")
                 .usingPort(4723)
+                .withArgument(GeneralServerFlag.BASEPATH, "/")
                 .withTimeout(Duration.ofSeconds(30))
-                .build().withBasePath("/");
+                .build();
         appiumServiceBuilder.start();
     }
 
